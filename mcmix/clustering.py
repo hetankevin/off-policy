6a725335c199e0b6bfde6eb796ec9e5912a5b0bf
hetankevin <kevtan@umich.edu>
2022-10-20 21:28:01 -0400

    update em algorithm

--- mcmix/clustering.py
+++ mcmix/clustering.py
@@ -26,21 +26,52 @@ def computeStat(h1, h2, eigvecsa, device='/CPU:0'):
     return statmns
 '''
 
-def computeStat(hs, eigvecsa, device='/CPU:0', proj=True):
-    with tf.device(device):
-        hst = tf.convert_to_tensor(hs, np.float64)
-        eigvecsat = tf.convert_to_tensor(eigvecsa, np.float64)
+def computeStat(hs, eigvecsa, numpy=True, smalldata=True, device='/CPU:0', proj=True):
+    if numpy:
         if proj:
-            projst = tf.squeeze(hst[..., None,:] @ eigvecsat[None,...])
+            projs = (hs[..., None,:] @ eigvecsa[None,...]).squeeze()
         else:
-            projst = hst
-        statmns2 = tf.reduce_max(
-                            tf.reduce_sum(
-                                (projst[0,None,...] - projst[0,:,None,...]) * 
-                                (projst[1,None,...] - projst[1,:,None,...]), 
-                            axis=-1), 
-                    axis=(2,3)).numpy()
-    return statmns2
+            projs = hs
+        if smalldata:
+            statmns = np.max(
+                        np.nansum(
+                            (projs[0,None,...] - projs[0,:,None,...]) * 
+                            (projs[1,None,...] - projs[1,:,None,...]), 
+                        axis=-1), 
+                    axis=(2,3))
+        else:
+            statmns = np.zeros((projs.shape[1], projs.shape[1]))
+            for s in tqdm(range(projs.shape[2])):
+                for a in range(projs.shape[3]):
+                    newstats = np.nansum((projs[0, None, :, s, a, :] - projs[0, :, None, s, a, :]) * 
+                                (projs[1, None, :, s, a, :] - projs[1, :, None, s, a, :]), -1)
+                    statmns = np.maximum(statmns, 
+                       newstats)
+        return statmns
+    else:
+        with tf.device(device):
+            hst = tf.convert_to_tensor(hs, np.float64)
+            eigvecsat = tf.convert_to_tensor(eigvecsa, np.float64)
+            if proj:
+                projst = tf.squeeze(hst[..., None,:] @ eigvecsat[None,...])
+            else:
+                projst = hst
+            if smalldata:
+                statmns = tf.reduce_max(
+                                    tf.reduce_sum(
+                                        (projst[0,None,...] - projst[0,:,None,...]) * 
+                                        (projst[1,None,...] - projst[1,:,None,...]), 
+                                    axis=-1), 
+                            axis=(2,3)).numpy()
+            else:
+                statmns = tf.zeros((projst.shape[1], projst.shape[1]), dtype=tf.double)
+                for s in tqdm(range(projst.shape[2])):
+                    for a in range(projst.shape[3]):
+                        newstats = tf.reduce_sum((projst[0, None, :, s, a, :] - projst[0, :, None, s, a, :]) * 
+                                    (projst[1, None, :, s, a, :] - projst[1, :, None, s, a, :]), -1)
+                        statmns = tf.math.maximum(statmns, newstats)
+                statmns = statmns.numpy()
+        return statmns
 
 ## OBTAINING CLUSTERS
 def getClusters(statmns, thresh, K, method='kmeans'):
--- mcmix/emalg.py
+++ mcmix/emalg.py
@@ -6,7 +6,61 @@ from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 import copy
 import helpers
 
-@njit(parallel=False, fastmath=True)
+
+# Gets starting state probabilities per confounder
+def getStartWeights(states, predlabs, K, nStates, hard=True):
+    wts = np.zeros((K, nStates))
+    if hard:
+        for i in range(len(states)):
+            wts[predlabs[i],states[i, 0]] += 1
+        return wts/np.array([sum(predlabs==k) for k in range(K)])[:,None]
+    else:
+        for i in range(len(states)):
+            for k in range(K):
+                wts[k,states[i, 0]] += predlabs[k, i]
+        return wts/np.sum(wts, axis=1)[:,None]
+
+@njit(parallel=False, cache=True)
+def getPolicyHelperSoft(states, actions, K, nStates, nActions, predprobs):
+    pi_ksa = np.zeros((K, nStates, nActions)) #k, s, a
+    for i in range(states.shape[0]):
+        for j in range(states.shape[1]):
+            for k in range(K):
+                pi_ksa[int(k), 
+                       int(states[i,j]), 
+                       int(actions[i,j])] += predprobs[k, i]
+    return pi_ksa
+
+@njit(parallel=False, cache=True)
+def getPolicyHelperLabs(states, actions, K, nStates, nActions, predlabs):
+    pi_ksa = np.zeros((K, nStates, nActions)) #k, s, a
+    for i in range(states.shape[0]):
+        for j in range(states.shape[1]):
+            pi_ksa[int(predlabs[i]), 
+                   int(states[i,j]), 
+                   int(actions[i,j])] += 1
+    return pi_ksa
+
+@njit(parallel=False, cache=True)
+def getPolicyHelper(states, actions, nStates, nActions):
+    pi_sa = np.zeros((nStates, nActions)) #s,a
+    for i in range(states.shape[0]):
+        for j in range(states.shape[1]):
+            pi_sa[int(states[i,j]), int(actions[i,j])] += 1
+    return pi_sa
+
+def getPolicyEstim(states, actions, K, nStates, nActions, preds=None, hard=True):
+    if not hard:
+        pi = getPolicyHelperSoft(states, actions, K, nStates, nActions, preds)
+    elif preds is not None:
+        pi = getPolicyHelperLabs(states, actions, K, nStates, nActions, preds)
+    else:
+        pi = getPolicyHelper(states, actions, nStates, nActions)
+    pi = pi/np.nansum(pi, axis=-1)[...,None]
+    pi[np.isnan(pi)] = 1/nActions
+    return pi
+
+@njit(parallel=False, cache=True)
 def getmodelestim(clusterlabs, states, actions, nextstates,
                   K, nStates, nActions):
     Phat_ksa = np.zeros((K, nStates, nActions, nStates))
@@ -18,7 +72,7 @@ def getmodelestim(clusterlabs, states, actions, nextstates,
                      int(nextstates[i,j])] += 1
     return Phat_ksa
 
-@njit(parallel=False, fastmath=True)
+@njit(parallel=False, cache=True)
 def getmodelestimsoft(expect, states, actions, nextstates,
                   K, nStates, nActions):
     Phat_ksa = np.zeros((K, nStates, nActions, nStates))
@@ -39,7 +93,9 @@ def getModelEstim(clusterlabs, states, actions, nextstates,
     else:
         model = getmodelestimsoft(clusterlabs, states, actions, nextstates,
                   K, nStates, nActions)
-    return model / np.nansum(model, axis=-1)[..., None]
+    model = model / np.nansum(model, axis=-1)[..., None]
+    model[np.isnan(model)] = 1/nStates
+    return model
 
 
 def getloglik(expect, modelestim, states, actions, nextstates, hard=True):
@@ -55,12 +111,15 @@ def getloglik(expect, modelestim, states, actions, nextstates, hard=True):
                                         * expect[...,None], axis=0)),
                     axis=-1))
 
-def classify(model, states, actions, nextstates, reg=0, prior=None, startweights=None, labs=True):
-    if startweights is not None:
-        probs = np.nansum(np.log(model[:, states, actions, nextstates]) 
-                          + np.log(startweights[:,states]), axis=-1)
+def classify(model, states, actions, nextstates, policy=None, reg=0, prior=None, startweights=None, labs=True):
+    if policy is not None:
+        probs = np.nansum(np.log(model[:, states, actions, nextstates])
+                    + np.log(policy[:, states, actions]), axis=-1)
     else:
         probs = np.nansum(np.log(model[:, states, actions, nextstates]), axis=-1)
+    if startweights is not None:
+        probs += np.log(startweights[:,states[:,0]])
+    
     probs += np.random.uniform(high=1e-7, size=probs.shape)
     if reg > 0:
         probs += reg*np.log(prior)[:,None]
@@ -71,39 +130,45 @@ def classify(model, states, actions, nextstates, reg=0, prior=None, startweights
     
 def em(expect, modelestim, states, actions, nextstates, labels, 
                 K, nStates, nActions, prior, 
-               max_iter = 100, checkin=5, reg = 0,
+               max_iter = 100, min_iter = 10, checkin=5, reg = 0,
                permute=False, permutation=0, verbose=True, hard=True):
     i = 0
     modelold = np.ones(modelestim.shape)
-    while i == 0 or np.nansum(np.abs(modelold - modelestim)) > 1e-3:
+    while i < min_iter or np.nansum(np.abs(modelold - modelestim)) > 1e-3:
         modelold = modelestim
         
         if hard:
+            policy = getPolicyEstim(states, actions, 
+                         K, nStates, nActions, expect, hard=True)
             modelestim = getModelEstim(expect.astype(int), states, actions, nextstates,
                                        K=K, nStates=nStates, nActions=nActions, hard=True)
-            startweights = helpers.getStartWeights(states, expect, K, nStates, hard=True)
+            startweights = getStartWeights(states, expect, K, nStates, hard=True)
             expectprobs = np.nansum(np.log(modelestim[:, states, actions, nextstates])
-                                    + np.log(startweights[:,states]), -1)
+                                    + np.log(policy[:, states, actions]), -1) + np.log(startweights[:,states[:,0]])
             expectprobs += np.random.uniform(high=1e-7, size=expectprobs.shape)
             expect = (expectprobs + #random number to perturb argmax 
                       reg*np.log(prior)[:,None]).argmax(0)
             prior = np.bincount(expect)/len(expect)
         else:
+            policy = getPolicyEstim(states, actions, 
+                         K, nStates, nActions, expect, hard=False)
             modelestim = getModelEstim(expect, states, actions, nextstates,
                                        K=K, nStates=nStates, nActions=nActions, hard=False)
-            startweights = helpers.getStartWeights(states, expect, K, nStates, hard=False)
-            expectprobs = np.nansum(np.log(modelestim[:, states, actions, nextstates]) 
-                                    + np.log(startweights[:,states]), axis=-1)
+            startweights = getStartWeights(states, expect, K, nStates, hard=False)
+            expectprobs = (np.nansum(np.log(modelestim[:, states, actions, nextstates]) 
+                                    + np.log(policy[:, states, actions]), axis=-1) 
+                           + np.log(startweights[:,states[:,0]]))
             expectprobs += np.random.uniform(high=1e-7, size=expectprobs.shape)
             expect = np.exp(expectprobs + 
                             reg*np.log(prior)[:,None])
             expect = (expect / np.nansum(np.abs(expect), axis=0))
-            prior = np.bincount(expect.argmax(0))/len(expect)
+            prior = np.bincount(expect.argmax(0))/len(expect) #fix to soft prior
         
         i += 1
         if i % checkin == 0 and verbose:
             print('iteration', i, 'diff', np.nansum(np.abs(modelold - modelestim)))
             expectlabs = expect if hard else expect.argmax(0)
+            print(len(expectlabs), len(labels))
             if permute:
                 print('accuracy:', max(np.mean(expectlabs == labels), np.mean(expectlabs != labels)))
                     
